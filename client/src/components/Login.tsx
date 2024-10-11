f7e03769965b069333c519f874ce4ee88505d82c
SerebryakovVV <serebryakov.valentin@gmail.com>
2024-10-11 19:27:01 +0700

    more login stuff

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,7 +3,7 @@ import ErrorMessage from "./ErrorMessage";
 
 
 type validationErrorType = "Password too short" | "Username too short";
-type authErrorType = "User not found" | "Wrong password";
+type authErrorType = "User not found" | "Wrong password" | "Server error";
 
 
 export default function Login() {
@@ -11,7 +11,7 @@ export default function Login() {
 	const [password, setPassword] = useState<string>("");
 	const [usernameError, setUsernameError] = useState<validationErrorType | "">("");
 	const [passwordError, setPasswordError] = useState<validationErrorType | "">("");
-	const [authError, setAuthError] = useState<authErrorType | "">("");
+	const [authError, setAuthError] = useState<authErrorType | "">("Server error");
 
 	const handleChangeUsername = (value: string): void => {
 		setUsername(/^[a-zA-Z0-9]{0,20}$/.test(value) ? value : username);
@@ -21,53 +21,80 @@ export default function Login() {
 		setPassword(/^[a-zA-Z0-9!@#$%&*]{0,20}$/.test(value) ? value : password);
 	}
 
-	const validateAndFetch = (): validationErrorType[] | void => {
+	const validateFields = (): void => {
 		setUsernameError("");
 		setPasswordError("");
 		setAuthError("");
 		if (username.length < 1) setUsernameError("Username too short");
 		if (password.length < 6) setPasswordError("Password too short");
-	
-		// fetch logic here
-
+		if (username.length < 1 || password.length < 6) return;
+		fetchLogin()
 	}
+
+	const fetchLogin = async (): Promise<void> => {
+		try {
+			console.log("heighla");
+			fetch("myapdfsfsdfsfsfefefefi.com", {});
+		} catch (err) {
+			console.log("its so over");
+		}
+
+
+		{/*
+			
+		async function fetchData() {
+  try {
+    const response = await fetch('https://api.example.com/data');
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json(); // Parse the response as JSON
+    console.log(data); // Use the fetched data
+
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+fetchData();	
+			
+		*/}
+
+
+	} 
 
   	return (
 		<div className="w-screen h-screen bg-zinc-100 flex justify-center items-center">
 			<div className="border border-solid border-black bg-white shadow-lg w-96 h-[500px] rounded-lg flex flex-col absolute">
 					<h1 className="text-3xl text-center mt-4 mb-12">Login</h1>
-					
-					
 					<div className="mx-7 h-24">
 						<input 
-						className="w-full mb-2 focus:outline-none border-b border-black" type="text" placeholder="Username" value={username}
-						onChange={(e)=>handleChangeUsername(e.target.value)}
+							className="w-full mb-2 focus:outline-none border-b border-black"
+							type="text" 
+							placeholder="Username" 
+							value={username}
+							onChange={(e)=>handleChangeUsername(e.target.value)}
 						/>
 						{usernameError && <ErrorMessage msg={usernameError}/>}
 					</div>
-					
-					
 					<div className="mx-7 h-24">
-
-					<input 
-						className="w-full mb-2 focus:outline-none border-b border-black" type="password" placeholder="Password" value={password}
-						onChange={(e)=>handleChangePassword(e.target.value)}
-					/>
-					{passwordError && <ErrorMessage msg={passwordError}/>}
-</div>
-					{/* {
-						errArr.map(el=><ErrorMessage msg={el}/>)
-					} */}
-
-<div className="mx-7 h-24">
-<ErrorMessage msg={"hello"}/>
-</div>
-	
-
-
+						<input 
+							className="w-full mb-2 focus:outline-none border-b border-black"
+							type="password" 
+							placeholder="Password" 
+							value={password}
+							onChange={(e)=>handleChangePassword(e.target.value)}
+						/>
+						{passwordError && <ErrorMessage msg={passwordError}/>}
+					</div>
+					<div className="mx-7 h-24">
+						{authError && <ErrorMessage msg={"hello"}/>}
+					</div>
 					<button 
 						className="mx-7 relative bottom-10 mt-auto rounded border border-solid border-black bg-black text-white hover:bg-white hover:text-black py-3"
-						onClick={validateAndFetch}
+						onClick={validateFields}
 					>
 						Submit
 					</button>
